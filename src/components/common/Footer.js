0dd2205937b46b532d6a755cb8bf04356423e934
Daniela Cardozo <mdanielacardozo@gmail.com>
2023-04-18 21:34:52 -0300

    Agregué carousel al Home

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  MDBFooter,
-  MDBContainer,
-  MDBCol,
-  MDBRow,
-} from "mdb-react-ui-kit";
+import { MDBFooter, MDBContainer, MDBCol, MDBRow } from "mdb-react-ui-kit";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -17,79 +12,77 @@ import "../common/Footer.css";
 
 const Footer = () => {
   return (
-    <div>
-      <MDBFooter
-        bgColor="dark"
-        className="text-center text-lg-start footerLetra"
-      >
-        <section className="pt-1">
-          <MDBContainer className="text-center text-md-start mt-5">
-            <MDBRow className="mt-3">
-              <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4">
-                <Link to="/">
-                  <img
-                    alt="Logo Cámara de Droguerías de Tucumán"
-                    src={Logo}
-                    width="170px"
-                    height="auto"
-                    className="my-0 py-0 ps-auto logoImg"
-                  />
+    <MDBFooter
+      bgColor="dark"
+      className="text-center text-lg-start footerLetra"
+    >
+      <section className="pt-1">
+        <MDBContainer className="text-center text-md-start mt-5">
+          <MDBRow className="pt-3">
+            <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4">
+              <Link to="/">
+                <img
+                  alt="Logo Cámara de Droguerías de Tucumán"
+                  src={Logo}
+                  width="170px"
+                  height="auto"
+                  className="my-0 py-0 ps-auto logoImg"
+                />
+              </Link>
+            </MDBCol>
+
+            <MDBCol md="2" lg="2" xl="2" className="mx-auto mb-4">
+              <h6 className=" fw-bold mb-4 fs-5">Conócenos</h6>
+              <p>
+                <Link to="/error" className="fs-6 text-reset subrayado">
+                  Contactanos
                 </Link>
-              </MDBCol>
+              </p>
+              <p>
+                <Link to="/error" className="fs-6 text-reset subrayado">
+                  Informacion legal
+                </Link>
+              </p>
+              <p>
+                <Link to="/error" className="fs-6 text-reset subrayado">
+                  Terminos y condiciones de uso
+                </Link>
+              </p>
+              <p>
+                <Link to="/error" className="fs-6 text-reset subrayado">
+                  Politica de privacidad
+                </Link>
+              </p>
+            </MDBCol>
+            <MDBCol md="4" lg="3" xl="3" className="mx-auto mb-md-0 mb-4">
+              <h6 className="fw-bold mb-4 fs-5">Redes sociales</h6>
+              <p>
+                <Link to="/error" className="fs-6 text-reset subrayado">
+                  <FontAwesomeIcon icon={faFacebook} /> Facebook
+                </Link>
+              </p>
+              <p>
+                <Link to="/error" className="fs-6 text-reset subrayado">
+                  <FontAwesomeIcon icon={faInstagram} /> Instagram
+                </Link>
+              </p>
+              <p>
+                <Link to="/error" className="fs-6 text-reset subrayado">
+                  <FontAwesomeIcon icon={faTwitter} /> Twitter
+                </Link>
+              </p>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </section>
 
-              <MDBCol md="2" lg="2" xl="2" className="mx-auto mb-4">
-                <h6 className=" fw-bold mb-4 fs-5">Conócenos</h6>
-                <p>
-                  <Link to="/error" className="fs-6 text-reset subrayado">
-                    Contactanos
-                  </Link>
-                </p>
-                <p>
-                  <Link to="/error" className="fs-6 text-reset subrayado">
-                    Informacion legal
-                  </Link>
-                </p>
-                <p>
-                  <Link to="/error" className="fs-6 text-reset subrayado">
-                    Terminos y condiciones de uso
-                  </Link>
-                </p>
-                <p>
-                  <Link to="/error" className="fs-6 text-reset subrayado">
-                    Politica de privacidad
-                  </Link>
-                </p>
-              </MDBCol>
-              <MDBCol md="4" lg="3" xl="3" className="mx-auto mb-md-0 mb-4">
-                <h6 className="fw-bold mb-4 fs-5">Redes sociales</h6>
-                <p>
-                  <Link to="/error" className="fs-6 text-reset subrayado">
-                    <FontAwesomeIcon icon={faFacebook} /> Facebook
-                  </Link>
-                </p>
-                <p>
-                  <Link to="/error" className="fs-6 text-reset subrayado">
-                    <FontAwesomeIcon icon={faInstagram} /> Instagram
-                  </Link>
-                </p>
-                <p>
-                  <Link to="/error" className="fs-6 text-reset subrayado">
-                    <FontAwesomeIcon icon={faTwitter} /> Twitter
-                  </Link>
-                </p>
-              </MDBCol>
-            </MDBRow>
-          </MDBContainer>
-        </section>
-
-        <div
-          className="text-center p-4"
-          style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
-        >
-          © Todos los derechos reservados.
-        </div>
-      </MDBFooter>
-    </div>
+      <div
+        className="text-center p-4"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
+      >
+        © Todos los derechos reservados.
+      </div>
+    </MDBFooter>
   );
 };
 
--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,16 +1,96 @@
-import React from 'react';
-import CarouselHome from "./CarouselHome";
+import React from "react";
 import "../views/Home.css";
+import { Carousel, Col, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBullhorn,
+  faCircleNodes,
+  faPeopleGroup
+} from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
-    return (
-        <div className='bgColor'>
-            <section>
-                <CarouselHome></CarouselHome>
-            </section>
-            
+  return (
+    <main className="bgColor">
+      <section className="CarouselPpal">
+        <Carousel>
+          <Carousel.Item className="portadaUno"></Carousel.Item>
+          <Carousel.Item className="portadaDos"></Carousel.Item>
+          <Carousel.Item className="portadaTres"></Carousel.Item>
+        </Carousel>
+      </section>
+      <section className="titulo py-3">
+        <div className=" w-100 d-flex justify-content-center text-light">
+          <h1 className="text-center">CÁMARA DE DROGUERÍAS DE TUCUMÁN</h1>
         </div>
-    );
+      </section>
+      <section className="iconos py-5">
+        <div className="container text-center">
+          <Row className="w-100 d-flex flex-wrap">
+            <Col
+              sm={12}
+              md={4}
+              lg={4}
+              className="d-flex justify-content-center"
+            >
+              <div>
+                <Link to={"/"}>
+                  <div className="iconoBg">
+                    <FontAwesomeIcon
+                      icon={faPeopleGroup}
+                    ></FontAwesomeIcon>
+                  </div>
+                </Link>
+                <div className="btnIconos">
+                  <Link to={"/"} className="btn text-white">
+                    <p className="h6 mt-1 fs-5">SOCIOS</p>
+                  </Link>
+                </div>
+              </div>
+            </Col>
+            <Col
+              sm={12}
+              md={4}
+              lg={4}
+              className="d-flex justify-content-center"
+            >
+              <div>
+                <Link to={"/"}>
+                  <div className="iconoBg">
+                    <FontAwesomeIcon icon={faCircleNodes}></FontAwesomeIcon>
+                  </div>
+                </Link>
+                <div className="btnIconos">
+                  <Link to={"/"} className="btn text-white">
+                    <p className="h6 mt-1 fs-5">SERVICIOS</p>
+                  </Link>
+                </div>
+              </div>
+            </Col>
+            <Col
+              sm={12}
+              md={4}
+              lg={4}
+              className="d-flex justify-content-center"
+            >
+              <div>
+                <Link to={"/"}>
+                  <div className="iconoBg">
+                    <FontAwesomeIcon icon={faBullhorn}></FontAwesomeIcon>
+                  </div>
+                </Link>
+                <div className="btnIconos">
+                  <Link to={"/"} className="btn text-white">
+                    <p className="h6 mt-1 fs-5">NOVEDADES</p>
+                  </Link>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Home;

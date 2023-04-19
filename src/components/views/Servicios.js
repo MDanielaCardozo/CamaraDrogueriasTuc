e7ef7ba128266ffed44f2c3d43546924d50b9d6f
Daniela Cardozo <mdanielacardozo@gmail.com>
2023-04-19 17:27:32 -0300

    Agregué la PagServicios

--- src/components/views/Servicios.js
+++ src/components/views/Servicios.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import React from "react";
+import { Row, Col, Card } from "react-bootstrap";
+import Semin from "../../img/seminariosEd.jpeg";
+import AseCont from "../../img/asesorCont.jpeg";
+import AseReg from "../../img/asesorReg.jpeg";
+import "../../components/views/Home.css";
 
 const Servicios = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="bgColor py-5">
+      <article className="container sitiosBg py-5">
+        <h2>SERVICIOS</h2>
+        <div className="my-5">
+          <Row>
+            <Col lg={4} md={6} sm={12} className="d-flex justify-content-center">
+              <Card className="p-1 mb-3 cardSocios">
+                <Card.Img variant="top" src={Semin} className="imgCard" />
+                <Card.Body className="p-1 text-center">
+                  <Card.Text>
+                    <h5 className="tituloNos">SEMINARIOS EDUCATIVOS</h5>
+                    <p></p>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col lg={4} md={6} sm={12} className="d-flex justify-content-center">
+              <Card className="p-1 mb-3 cardSocios">
+                <Card.Img variant="top" src={AseCont} className="imgCard" />
+                <Card.Body className="p-1 text-center">
+                  <Card.Text>
+                    <h5 className="tituloNos">ASESORAMIENTO CONTABLE</h5>
+                    <p></p>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col lg={4} md={6} sm={12} className="d-flex justify-content-center">
+              <Card className="p-1 mb-3 cardSocios">
+                <Card.Img variant="top" src={AseReg} className="imgCard" />
+                <Card.Body className="p-1 text-center">
+                  <Card.Text>
+                    <h5 className="tituloNos">ASESORAMIENTO REGULADOR</h5>
+                    <p></p>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
         </div>
-    );
+      </article>
+    </div>
+  );
 };
 
 export default Servicios;
--- src/components/views/SitiosOficiales.js
+++ src/components/views/SitiosOficiales.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import "../views/SitiosOficiales.css";
+import { Col, Row } from 'react-bootstrap';
+import Minist from "../../img/logominsalud.jpg";
+import Anmat from "../../img/logoAnmat.jpg";
+import SuperIn from "../../img/logoSuper.jpg";
+
 
 const SitiosOficiales = () => {
     return (
-        <div>
-            
+        <div className='bgColor py-5'>
+            <article className='container sitiosBg py-5'>
+                <h2>SITIOS OFICIALES</h2>
+                <div>
+                    <Row>
+                        <Col><img src={Minist} alt="" className=''/></Col>
+                        <Col><img src={Anmat} alt="" className=''/></Col>
+                        <Col><img src={SuperIn} alt="" className=''/></Col>
+                    </Row>
+                </div>
+            </article>
         </div>
     );
 };

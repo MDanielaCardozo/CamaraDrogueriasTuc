4bcbad7c718b443dd9e923f1eb1101fbb00ca155
Daniela Cardozo <mdanielacardozo@gmail.com>
2023-05-30 19:15:45 -0300

    Agregué la logica para EmailJs en la pagina contacto

--- src/components/views/Contacto.js
+++ src/components/views/Contacto.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 // import { useState } from "react";
 import "../views/Contacto.css";
 import { Row, Col, Form, Button } from "react-bootstrap";
@@ -9,10 +9,32 @@ import {
   faPhone,
 } from "@fortawesome/free-solid-svg-icons";
 import MapaGoogle from "./MapaGoogle";
+import emailjs from "@emailjs/browser";
 
 const Contacto = () => {
   // const [nombre, setnombre] = useState("");
   // const [email, setemail] = useState("");
+
+  const refForm = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const EMAILJS_SERVICE = process.env.REACT_APP_EMAILJS_SERVICE;
+    const EMAILJS_TEMPLATE = process.env.REACT_APP_EMAILJS_TEMPLATE;
+    const EMAILJS_PUBLICKEY = process.env.REACT_APP_EMAILJS_PUBLICKEY;
+
+    emailjs
+      .sendForm(EMAILJS_SERVICE, EMAILJS_TEMPLATE, refForm.current, EMAILJS_PUBLICKEY)
+      .then((result) => {
+        console.log(result.text);
+        alert("El mensaje fue enviado exitosamente");
+      })
+      .catch((error) => {
+        console.log(error.text);
+        alert("El mensaje no pudo ser enviado");
+      });
+  };
 
   return (
     <div className="bgColor py-5">
@@ -35,48 +57,54 @@ const Contacto = () => {
                 icon={faEnvelope}
                 className="iconoContacto mx-3"
               ></FontAwesomeIcon>
-              <p className="textoC">
-                camaradedrogueriastucuman@gmail.com
-              </p>
+              <p className="textoC">camaradedrogueriastucuman@gmail.com</p>
             </div>
             <div className="d-flex">
               <FontAwesomeIcon
                 icon={faPhone}
                 className="iconoContacto mx-3"
               ></FontAwesomeIcon>
-              <p className="textoC">
-                381-6833503
-              </p>
+              <p className="textoC">381-6833503</p>
             </div>
           </Col>
           <Col lg={6} md={6} sm={12} className="my-5">
-            <Form.Group>
-              <Form.Label className="">Nombre Completo*</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="entre (8 y 40) caracteres."
-                // onChange={(e) => setnombre(e.target.value.trim())}
-              />
-              <Form.Label className="my-2">Email *</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Ej: juanperez@gmail.com"
-                // onChange={(e) => setemail(e.target.value.trim())}
-              />
-              <Form.Label className="my-2">Mensaje</Form.Label>
-              <Form.Control as="textarea" rows={3} />
-            </Form.Group>
-            <Button variant="outline-dark" type="submit" className="my-4 me-0">
-              Enviar
-            </Button>
+            <Form ref={refForm} onSubmit={handleSubmit}>
+              <Form.Group>
+                <Form.Label className="">Nombre Completo*</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="entre (8 y 40) caracteres."
+                  name="from_name"
+                  required
+                  // onChange={(e) => setnombre(e.target.value.trim())}
+                />
+                <Form.Label className="my-2">Email *</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Ej: juanperez@gmail.com"
+                  name="user_email"
+                  required
+                  // onChange={(e) => setemail(e.target.value.trim())}
+                />
+                <Form.Label className="my-2">Mensaje</Form.Label>
+                <Form.Control as="textarea" rows={3} name="message" required />
+              </Form.Group>
+              <Button
+                variant="outline-dark"
+                type="submit"
+                className="my-4 me-0"
+              >
+                Enviar
+              </Button>
+            </Form>
           </Col>
         </Row>
       </section>
       <section className="container">
-      <div className='mb-5 text-secondary'>
-                <h2>MAPA</h2>
-                <hr />
-                </div>
+        <div className="mb-5 text-secondary">
+          <h2>MAPA</h2>
+          <hr />
+        </div>
         <MapaGoogle></MapaGoogle>
       </section>
     </div>

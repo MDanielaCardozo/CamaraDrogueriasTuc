ced03dd700e49b24450cc13af60fbe44ce7ce23b
Daniela Cardozo <mdanielacardozo@gmail.com>
2023-07-04 13:02:50 -0300

    Agregué Página error 404

---
+++ src/components/views/Error.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <section className='page404'>
+            <div className='container'> 
+               <Row>
+                <Col>
+                <div>
+                    <h1>OOOPS!</h1>
+                    <p>PAGE NOT FOUND</p>
+                </div>
+                </Col>
+               </Row>
+            </div>
+        </section>
+    );
+};
+
+export default Error;
